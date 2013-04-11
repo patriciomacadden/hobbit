a9f06657435816aa3d5b3659ac177211dd5c81d6
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-04-11 20:21:55 -0300

    use Tilt::Cache

--- lib/bonsai/render.rb
+++ lib/bonsai/render.rb
@@ -3,7 +3,15 @@ require 'tilt'
 module Bonsai
   module Render
     def render(template, locals = {}, options = {}, &block)
-      Tilt.new(template, options).render(self, locals, &block)
+      cache.fetch(template) do
+        Tilt.new(template, options)
+      end.render(self, locals, &block)
+    end
+
+    private
+
+    def cache
+      Thread.current[:cache] ||= Tilt::Cache.new
     end
   end
 end
