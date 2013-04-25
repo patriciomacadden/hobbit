1fb98fadaf746a24c0c66cf88a2cb2b02588431c
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-04-25 15:28:24 -0300

    initialize Hobbit::Response with text/html; charset=utf-8

--- lib/hobbit/response.rb
+++ lib/hobbit/response.rb
@@ -3,7 +3,7 @@ require 'rack'
 module Hobbit
   class Response < Rack::Response
     def initialize(body = [], status = 200, header = {})
-      header['Content-Type'] = 'text/html'
+      header['Content-Type'] = 'text/html; charset=utf-8'
       super
     end
   end
--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -2,8 +2,8 @@ require 'minitest_helper'
 
 describe Hobbit::Response do
   describe '#initialize' do
-    it 'must initialize Content-Type with text/html' do
-      Hobbit::Response.new.headers['Content-Type'].must_equal 'text/html'
+    it 'must initialize Content-Type with text/html; charset=utf-8' do
+      Hobbit::Response.new.headers['Content-Type'].must_equal 'text/html; charset=utf-8'
     end
   end
 end
