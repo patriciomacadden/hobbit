7acdbac013a8ff1558908be6a02cf46348a75758
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-12-14 18:01:48 -0300

    Hobbit::Response is no longer a subclass of Rack::Response

--- lib/hobbit/response.rb
+++ lib/hobbit/response.rb
@@ -1,10 +1,22 @@
-require 'rack'
+require 'forwardable'
 
 module Hobbit
-  class Response < Rack::Response
-    def initialize(body = [], status = 200, header = {})
-      header['Content-Type'] = 'text/html; charset=utf-8'
-      super
+  class Response
+    attr_accessor :body, :headers, :length, :status
+    extend Forwardable
+    def_delegators :headers, :[], :[]=
+
+    def initialize(body = [], status = 200, headers = { 'Content-Type' => 'text/html; charset=utf-8' })
+      @body, @headers, @status = body, headers, status
+    end
+
+    def finish
+      headers['Content-Length'] = body.each.map(&:size).inject { |memo, current| memo += current }
+      [status, headers, body]
+    end
+
+    def write(string)
+      self.body << string
     end
   end
 end
--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -2,8 +2,73 @@ require 'minitest_helper'
 
 describe Hobbit::Response do
   describe '#initialize' do
-    it 'must initialize Content-Type with text/html; charset=utf-8' do
-      Hobbit::Response.new.headers['Content-Type'].must_equal 'text/html; charset=utf-8'
+    it 'must set the body, status and headers with no arguments given' do
+      default_headers = { 'Content-Type' => 'text/html; charset=utf-8' }
+      response = Hobbit::Response.new
+      response.status.must_equal 200
+      response.headers.must_equal default_headers
+      response.body.must_equal []
+    end
+
+    it 'must set the body, status and headers with arguments given' do
+      status, headers, body = 200, { 'Content-Type' => 'application/json' }, ['{"name":"Hobbit"}']
+      response = Hobbit::Response.new body, status, headers
+      response.status.must_equal status
+      response.headers.must_equal headers
+      response.body.must_equal body
+    end
+  end
+
+  describe '#[]' do
+    let(:response) { Hobbit::Response.new }
+
+    it 'must respond to #[]' do
+      response.must_respond_to :[]
+    end
+
+    it 'must return a header' do
+      response['Content-Type'].must_equal 'text/html; charset=utf-8'
+    end
+  end
+
+  describe '#[]=' do
+    let(:response) { Hobbit::Response.new }
+
+    it 'must respond to #[]=' do
+      response.must_respond_to :[]=
+    end
+
+    it 'must set a header' do
+      content_type = 'text/html; charset=utf-8'
+      response['Content-Type'] = content_type
+      response['Content-Type'].must_equal content_type
+    end
+  end
+
+  describe '#finish' do
+    let(:status) { 200 }
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+    let(:body) { ['{"name":"Hobbit"}'] }
+
+    it 'must return a 3 elements array with status, headers and body' do
+      response = Hobbit::Response.new body, status, headers
+      response.finish.must_equal [status, headers, body]
+    end
+
+    it 'must calculate the Content-Length of the body' do
+      response = Hobbit::Response.new body, status, headers
+      s, h, b = response.finish
+      h.must_include 'Content-Length'
+      h['Content-Length'].must_equal body.each.map(&:size).inject { |m, s| m += s }
+    end
+  end
+
+  describe '#write' do
+    let(:response) { Hobbit::Response.new }
+
+    it 'must append the argument to the body of the response' do
+      response.write 'hello world'
+      response.body.must_equal ['hello world']
     end
   end
 end
